20f82ecde206e7cf4fdd1e70e850a02a424975ee
dahunsi halima <dahunsiolajumoke18@gmail.com>
2025-09-18 07:49:21 +0100

    mobile resonsiveness updated

--- src/home.jsx
+++ src/home.jsx
@@ -7,6 +7,7 @@ import {
   BsFillEnvelopeFill,
   BsGithub,
   BsLinkedin,
+  BsStar,
 } from "react-icons/bs";
 import { HiHandRaised } from "react-icons/hi2";
 import { GrUserFemale } from "react-icons/gr";
@@ -14,22 +15,25 @@ import { AiOutlineClose } from "react-icons/ai";
 
 function Home() {
   const [hoverProject, setHoverProject] = useState(false);
- // project()
+ // project lists
  const projects =[
   {project: 'Task pad', link:'https://lymarh-taskpad.netlify.app/', Image:"/taskpad image.png", id:1, description:"this is a to do app", technology:'React.js, SAAS, Git and github, deployed on Netlify'},
   {project: 'Multi step form',link:'https://multii-form.netlify.app/', Image:"/multi-form.png", id:2, description:"This is a multi step form", technology:'React.js, SAAS, Git and github, deployed on Netlify'},
   {project: 'Quiz app',link:'https://trivia-twist.netlify.app/', Image:"/trivia.png", id:3, description:"This is a Quiz app",technology:'HTML5, CSS3, Git and github, deployed on Netlify'},
-  {project: 'ProNet',link:'https://pro-net.onrender.com/', Image:"/pronet.png",  id:4, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'},
-  {project: 'Ticket generator',link:'https://ticket-generator-5aug.onrender.com/', Image:"/ticket-generator.png",  id:5, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'},
-  {project: 'Color game',link:'https://colorgame-05k0.onrender.com', Image:"/color-game.png",  id:6, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'}
+  {project: 'Product page',link:'https://halima-1.github.io/web3Bridge_product_details/', Image:"/product-details.png",  id:4, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'},
+  {project: 'ProNet',link:'https://pro-net.onrender.com/', Image:"/pronet.png",  id:5, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'},
+  {project: 'Ticket generator',link:'https://ticket-generator-5aug.onrender.com/', Image:"/tickhttps://halima-1.github.io/web3Bridge_product_details/et-generator.png",  id:6, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'},
+  {project: 'Color game',link:'https://colorgame-05k0.onrender.com', Image:"/color-game.png",  id:7, description:"This is a linkedIn clone", technology:'React.js, SAAS, Git and github, deployed on Render'}
 ]
 // localStorage.removeItem('activeProject')
+// setting a clicked project to local storage to display it details
 const [activeItems, setActiveItems] = useState(0);
 if(localStorage.getItem('activeProject')
 ){null}
 else{localStorage.setItem('activeProject', JSON.stringify(projects[0]))
 }
 localStorage.setItem('projects', JSON.stringify(projects))
+// project displaying a project is clicked
   const project = (id) => {
     if(JSON.parse(localStorage.getItem('activeProject')).id ===id){
       setHoverProject(!hoverProject)
@@ -40,9 +44,8 @@ localStorage.setItem('projects', JSON.stringify(projects))
      localStorage.setItem('activeProject', JSON.stringify(activeProject))
     // console.log(projectClick)
     console.log(activeProject)
-    // const style = JSON.parse(localStorage.getItem('activeProject')).id ===id? {display:""}
-    // window.location.href ='/'
   };
+  // to close project display
   const closeIcon =()=>{
     setHoverProject(!hoverProject)
 
@@ -50,12 +53,15 @@ localStorage.setItem('projects', JSON.stringify(projects))
   const atvProjectDisplay =JSON.parse(localStorage.getItem('activeProject'))
   return (
     <>
-      {/* <h1>My portfolio</h1> */}
       <header>
         <div>
           <h1>
-            <HiHandRaised />
-            {/* <GrUserFemale /> */}
+            <BsStar />
+            <BsStar />
+            <BsStar />
+            <BsStar />
+            <BsStar />
+
           </h1>
         </div>
         <nav>
@@ -77,20 +83,14 @@ localStorage.setItem('projects', JSON.stringify(projects))
         <section className="intro">
           <h1>Welcome!</h1>
           <p>
-            Hi, I am Dahunsi Halima Olajumoke, a frontend web developer based in
-            Ibadan, Oyo State, Nigeria.
-          </p>
-          <p>
-            I am passionate about building a responsive and fascinating web
-            interface with high scalablity and facilitating great user
-            experience.
-          </p>
-
-          <p>
-            Do you need a capable and smart frontend developer to join your
-            team? I will be glad to contribute to the growth of your team too.
-          </p>
-
+            Hi, I am  Halima Dahunsi, Frontend Developer skilled in JavaScript, React,
+             and modern CSS frameworks. I specialize in building responsive websites, 
+            dynamic interfaces, and interactive applications that bring ideas to life.
+            For me, frontend development is more than code — it’s about creating meaningful
+             digital experiences. I design and build websites that
+             not only look great but also solve real problems for users and businesses.
+             Looking for someone who can turn designs into real, working products? I’d love to help.
+            </p>
           <a
             className="btn"
             href="https://docs.google.com/document/d/1VEmdD9m2WNQHkEEZldhDPXPwOlGJtoxiwr0hVwucgSM/edit?usp=sharing"
@@ -100,7 +100,7 @@ localStorage.setItem('projects', JSON.stringify(projects))
           </a>
         </section>
         <section>
-          <h1>My projects</h1>
+          <h1>My recent projects</h1>
           <div className="project">
           <div className="shape"></div>
           {
@@ -134,12 +134,14 @@ localStorage.setItem('projects', JSON.stringify(projects))
       <footer>
         <nav></nav>
         <div>
-          <p>
-            2024 Halima
-            {/* <a href="mailto:dahunsiolajumoke18@gmail.com">
-              dahunsiolajumoke18@gmail.com
-            </a> */}
-          </p>
+        <h1>
+            <BsStar />
+            <BsStar />
+            <BsStar />
+            <BsStar />
+            <BsStar />
+
+          </h1>
         </div>
       </footer>
     </>
